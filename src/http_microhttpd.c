6d95ffe913f601dd501ca3d656e850698a2e06d1
Alexander Couzens <lynxis@fe80.eu>
2015-04-02 10:31:08 +0200

    microhttpd: add send_error

--- src/http_microhttpd.c
+++ src/http_microhttpd.c
@@ -22,8 +22,9 @@ libmicrohttpd_cb(void *cls,
   s_config *config;
   t_client *client;
 
+  /* only allow get */
   if (0 != strcmp (method, "GET"))
-    return MHD_NO;              /* unexpected method */
+    return send_error(connection, 503);
 
   /* switch between preauth, authenticated */
   /* - always - set caching headers
@@ -82,4 +83,48 @@ add_client(struct MHD_Connection *connection)
   client = client_list_add_client(ip_addr);
   UNLOCK_CLIENT_LIST();
   return client;
+}
+
+int send_error(struct MHD_Connection *connection, int error)
+{
+  struct MHD_Response *response;
+  // cannot automate since cannot translate automagically between error number and MHD's status codes -- and cannot rely on MHD_HTTP_ values to provide an upper bound for an array
+  const char *page_400 = "<html><head><title>Error 400</title></head><body><h1>Error 400 - Bad Request</h1></body></html>";
+  const char *page_403 = "<html><head><title>Error 403</title></head><body><h1>Error 403 - Forbidden</h1></body></html>";
+  const char *page_404 = "<html><head><title>Error 404</title></head><body><h1>Error 404 - Not Found</h1></body></html>";
+  const char *page_500 = "<html><head><title>Error 500</title></head><body><h1>Error 500 - Internal Server Error. Oh no!</body></html>";
+  const char *page_501 = "<html><head><title>Error 501</title></head><body><h1>Error 501 - Not Implemented</h1></body></html>";
+
+  int ret;
+
+  switch (error)
+  {
+  case 400:
+    response = MHD_create_response_from_data(strlen(page_400), (char *)page_400, MHD_NO, MHD_NO);
+    ret = MHD_queue_response(connection, MHD_HTTP_BAD_REQUEST, response);
+    break;
+
+  case 403:
+    response = MHD_create_response_from_data(strlen(page_403), (char *)page_403, MHD_NO, MHD_NO);
+    ret = MHD_queue_response(connection, MHD_HTTP_FORBIDDEN, response);
+    break;
+
+  case 404:
+    response = MHD_create_response_from_data(strlen(page_404), (char *)page_404, MHD_NO, MHD_NO);
+    ret = MHD_queue_response(connection, MHD_HTTP_NOT_FOUND, response);
+    break;
+
+  case 500:
+    response = MHD_create_response_from_data(strlen(page_500), (char *)page_500, MHD_NO, MHD_NO);
+    ret = MHD_queue_response(connection, MHD_HTTP_INTERNAL_SERVER_ERROR, response);
+    break;
+
+  case 501:
+    response = MHD_create_response_from_data(strlen(page_501), (char *)page_501, MHD_NO, MHD_NO);
+    ret = MHD_queue_response(connection, MHD_HTTP_NOT_IMPLEMENTED, response);
+    break;
+  }
+
+  MHD_destroy_response(response);
+  return ret;
 }

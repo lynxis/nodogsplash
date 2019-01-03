a672a2ad18f930a5d01e11080eac9a9815c51900
Alexander Couzens <lynxis@fe80.eu>
2019-01-03 18:26:16 +0100

    WIP: prepare_server: ip addr

--- tests/prepare_server.sh
+++ tests/prepare_server.sh
@@ -16,7 +16,7 @@ make clean
 make
 make install
 
-ip addr 192.168.250.2/24 dev eth0
+ip addr add 192.168.250.2/24 dev eth0
 ip route add default via 192.168.250.1
 
-ip addr 192.168.55.1/24 dev eth1
+ip addr add 192.168.55.1/24 dev eth1

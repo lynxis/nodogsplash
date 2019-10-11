b923fe7dd019697ce4cd8990c13e4376c258bc6d
Alexander Couzens <lynxis@fe80.eu>
2019-10-11 17:41:17 +0200

    5!

--- tests/run_httpd.sh
+++ tests/run_httpd.sh
@@ -1,11 +1,3 @@
 #!/bin/sh
 #
-# arguments must contain a server like '-b 127.0.0.1:8942'
-# but it could also contains additional arguments like '-a'
-
-cd /srv/tunneldigger/client
-if [ -f CMakeLists.txt ]; then
-	exec /srv/tunneldigger/client/tunneldigger -u foobar -i l2tp0 -t 2 $SERVERS -L 102400 -s /testing/hook_client.sh -f $@
-else
-	exec /srv/tunneldigger/client/l2tp_client -u foobar -i l2tp0 -t 2 $SERVERS -L 102400 -s /testing/hook_client.sh -f $@
-fi
+echo "httpd!"
--- tests/run_nds.sh
+++ tests/run_nds.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-/srv/nodogsplash/nodogsplash
+set -e
+
+export LD_LIBRARY_PATH=/tmp/libmicrohttpd_install/lib
+cd /srv/nodogsplash/
+./nodogsplash -c tests/nodogsplash.conf
